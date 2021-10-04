b4fa477e91e04f68f6165ac85e991be54a5c0ac1
tarunwalia12345 <57227656+tarunwalia12345@users.noreply.github.com>
2021-10-04 23:25:58 +0530

    CarRacingGame

---
+++ Assets/AssetsByME/Scripts/Countdown.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+    public GameObject CountDown;
+    public AudioSource GetReady;
+    public AudioSource GoAudio;
+    public GameObject LapTimer;
+   // public GameObject CarControls;
+
+    // trun on sound sabek and la7ek in the start of the gaem
+    public AudioSource LevelMusic;
+    void Start()
+    {
+        StartCoroutine(CountStart());
+    }
+
+
+    IEnumerator CountStart()
+    {
+        //yield return new WaitForSeconds(0.5f);
+        CountDown.GetComponent<Text>().text = "3";
+        GetReady.Play();
+        CountDown.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        CountDown.SetActive(false);
+
+        CountDown.GetComponent<Text>().text = "2";
+        GetReady.Play();
+        CountDown.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        CountDown.SetActive(false);
+        CountDown.GetComponent<Text>().text = "1";
+        GetReady.Play();
+        CountDown.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        CountDown.SetActive(false);
+        GoAudio.Play();
+        LevelMusic.Play();
+        LapTimer.SetActive(true);
+
+    }
+
+}
---
+++ Assets/AssetsByME/Scripts/LapComplete.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapComplete : MonoBehaviour
+{
+
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MilliDisplay;
+
+    public GameObject LapTimeBox;
+    // count the laps that was compeleted
+    public GameObject LapCounter;
+    public int LapsDone;
+    void OnTriggerEnter()
+    {
+        LapsDone += 1;
+        if (LapTimeManager.SecondCount <= 9)
+        {
+            SecondDisplay.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
+        }
+        else
+        {
+            SecondDisplay.GetComponent<Text>().text = "" + LapTimeManager.SecondCount + ".";
+        }
+
+        if (LapTimeManager.MinuteCount <= 9)
+        {
+            MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManager.MinuteCount + ".";
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManager.MinuteCount + ".";
+        }
+
+        MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
+
+        // added by Ahmed Shaaban
+        PlayerPrefs.SetInt("MinSave",LapTimeManager.MinuteCount);
+        PlayerPrefs.SetInt("SecSave",LapTimeManager.SecondCount);
+        PlayerPrefs.SetFloat("MilliSave",LapTimeManager.MilliCount);
+
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MilliCount = 0;
+        ///
+        LapCounter.GetComponent<Text>().text = "" + LapsDone;
+
+        HalfLapTrig.SetActive(true);
+        LapCompleteTrig.SetActive(false);
+    }
+
+}
